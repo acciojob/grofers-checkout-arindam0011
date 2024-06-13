2f3bc8540bd4ba5f34ffd08d8e55db9e85ad6c6f
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-13 18:46:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let table= document.querySelector('table');
+	let total=table.querySelectorAll('.price');
+ let totalSum=0;
+	for(let t of total){
+		totalSum+=parseInt(t.innerText);
+	}
+  let newTotal= document.createElement("div");
+
+newTotal.innerHTML=`<p>${totalSum}</p>`
+
+document.body.appendChild(newTotal);
 };
 
 getSumBtn.addEventListener("click", getSum);
